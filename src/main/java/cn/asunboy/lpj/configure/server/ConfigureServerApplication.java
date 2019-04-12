c7fc98ac1fb64da2335dbd84fd61caee98b7ddf3
lipengju <921295877@qq.com>
2019-04-13 00:24:26 +0800

    新建项目

---
+++ src/main/java/cn/asunboy/lpj/configure/server/ConfigureServerApplication.java
@@ -0,0 +1,15 @@
+package cn.asunboy.lpj.configure.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigureServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigureServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/asunboy/lpj/configure/server/ConfigureServerApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.asunboy.lpj.configure.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigureServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
